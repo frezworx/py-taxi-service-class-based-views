baf9a592811a7dd4dc965b01180fc69195ef9dce
Frezworx <frezworx@gmail.com>
2024-10-18 19:34:43 +0300

    Add driver, car, and manufacturer detail and list views
    Implemented views and templates for driver, car, and manufacturer details and lists. Enhanced base template with pagination includes and fixed URL links in the sidebar.

--- taxi/views.py
+++ taxi/views.py
@@ -1,4 +1,7 @@
+from symtable import Class
+
 from django.shortcuts import render
+from django.views import generic
 
 from taxi.models import Driver, Car, Manufacturer
 
@@ -13,3 +16,36 @@ def index(request):
     }
 
     return render(request, "taxi/index.html", context=context)
+
+
+class ManufacturerListView(generic.ListView):
+    model = Manufacturer
+    queryset = Manufacturer.objects.all().order_by("name")
+    paginate_by = 5
+    context_object_name = "manufacturer_list"
+
+
+class CarsListView(generic.ListView):
+    model = Car
+    queryset = Car.objects.select_related("manufacturer").order_by("id")
+    paginate_by = 5
+    context_object_name = "car_list"
+
+
+class CarDetailView(generic.DetailView):
+    model = Car
+    queryset = Car.objects.prefetch_related("drivers")
+
+
+class DriverListView(generic.ListView):
+    model = Driver
+    queryset = Driver.objects.all().order_by("id")
+    paginate_by = 5
+    context_object_name = "driver_list"
+
+
+class DriverDetailView(generic.DetailView):
+    model = Driver
+    queryset = Driver.objects.prefetch_related(
+        "cars__manufacturer"
+    ).order_by("id")
